2565078b247a6cb6e213ddb7d677668bb97c6876
Xiaochan Xia <xiaochan.xia@qq.com>
2019-03-09 15:34:12 +0800

    add index.js

---
+++ src/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: 'airbnb',
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    worker: true,
+  },
+  globals: {
+    window: true,
+    document: true,
+  },
+  rules: {
+    strict: 'error',
+    semi: ['error', 'always'],
+    'semi-style': ['error', 'last'],
+    'no-unused-vars': 'error',
+    'import/prefer-default-export': 0,
+    'react/jsx-filename-extension': 0,
+    'no-console': 0,
+  },
+};
